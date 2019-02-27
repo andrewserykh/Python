b5c2ff54483117b39476df7a79203fd3f0aea151
Andrew Serykh <andrew.serykh@gmail.com>
2019-02-27 23:31:33 +0700

    wsclient

--- bankturov.py
+++ bankturov.py
@@ -1,6 +1,6 @@
 import json, requests
 
-url = 'http://search.bankturov.ru/search/?SID=null&departure_id=1281&destination_id=59&adult_num=2&child_num=1&child_ages%5B%5D=0&arrivalDateFrom=02.03.2019&arrivalDateTo=16.03.2019&nonstop=true&ticket=true&nightsStart=18&nightsEnd=22&hotelStars=&hotels%5B%5D=39564&mealType=&valute=RUB&offer_currency=RUB&source=search_online_page_otour&_=1542555642619'
+url = 'http://search.bankturov.ru/search/?SID=null&departure_id=1281&destination_id=59&adult_num=2&child_num=1&child_ages=2%5B%5D=0&arrivalDateFrom=28.03.2019&arrivalDateTo=29.03.2019&nonstop=true&ticket=true&nightsStart=18&nightsEnd=22&hotelStars=&hotels%5B%5D=39564&mealType=&valute=RUB&offer_currency=RUB&source=search_online_page_otour&_=1542555642619'
 
 data = requests.get(url=url)
 binary = data.content
@@ -8,14 +8,15 @@ jres = json.loads(binary)
 cost = 0
 
 for data in jres['data']:
-    if data['residenses']['room_name']=="Deluxe Garden View B (with balcony)":
-        print data['date']
-        print data['residenses']['original_hotel_name']
-        print data['residenses']['room_name']
-        print data['costValues']['RUB']['source'],"RUB"
-        print data['costValues']['RUB']['surcharged'],"RUB"
-        print "-"
-        cost = data['costValues']['RUB']['source']
+    #print data['residenses']['room_name'] 
+    if data['residenses']['room_name']=="Superior Garden View A":
+    	print data['date']
+    	print data['residenses']['original_hotel_name']
+    	print data['residenses']['room_name']
+    	print data['costValues']['RUB']['source'],"RUB"
+    	print data['costValues']['RUB']['surcharged'],"RUB"
+    	print "-"
+    	cost = data['costValues']['RUB']['surcharged']
 
 maxcost = 0
 mincost = 999999
